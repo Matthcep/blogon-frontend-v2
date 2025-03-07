bcde64a243983795801294e21b9a88aaea9eb745
Matthew Cepeda <matthew@Matthews-MacBook-Pro.local>
2025-03-06 21:06:35 -0500

    Pagina de blogs principal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { BlogService } from 'app/services/blog.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CategoryService } from 'app/services/category.service';
+import Swal from 'sweetalert2'
+
 
 @Component({
   selector: 'app-home',
@@ -17,23 +19,20 @@ export class HomeComponent implements OnInit {
   blogId: number = 0;
   blogDetails: any;
 
+  showForm = false;
+  blogSelected: any;
+  title: any;
+  isEditing = false;
+
   constructor(private blogService: BlogService, private fb: FormBuilder, private categoryService: CategoryService) { }
 
   ngOnInit() {
-
-    this.blogService.getBlogs().subscribe((data) => {
-      this.blogs = data;
-      console.log(this.blogs);
-      
-    });
 
     this.categoryService.getCategories().subscribe((data) => {
       this.categories = data;
       console.log(this.categories);
       
     });
-
-
 
     this.blogForm = this.fb.group({
       title: ['', Validators.required],
@@ -42,26 +41,78 @@ export class HomeComponent implements OnInit {
       content: ['', Validators.required],  
       category: [null, Validators.required]
     });
+
+    this.uploadBlogs();
       
+  }
+
+  uploadBlogs() {
+    this.blogService.getBlogs().subscribe((data) => {
+      this.blogs = data;
+    });
   }
 
   onSubmit(): void {
     // Maneja el envío del formulario
     if (this.blogForm.valid) {
       const updatedBlog = this.blogForm.value;
-      this.blogService.updateBlog(this.blogId, updatedBlog).subscribe(
+
+      console.log('Blog actualizado:', updatedBlog);
+      console.log(updatedBlog.category.id );
+      
+      updatedBlog.id = this.blogSelected?.id;
+
+      this.blogService.saveBlog(updatedBlog, this.isEditing).subscribe(
         (response) => {
-          console.log('Blog actualizado:', response);
-          alert('Blog actualizado exitosamente');
+          console.log('Blog actualizado:', response);          
+          this.blogForm.reset();
+          this.uploadBlogs();
+          Swal.fire({
+            title: 'Información',
+            text: 'El blog se ha creado correctamente',
+            icon: 'success',
+            confirmButtonText: 'OK'
+          });
+          this.showForm = false;
         },
         (error) => {
-          console.error('Error al actualizar el blog:', error);
+          Swal.fire({
+            title: 'Error',
+            text: 'Ha ocurrido un error al crear el blog',
+            icon: 'error',
+            confirmButtonText: 'OK'
+          })
         }
       );
     }
   }
 
-  // Maneja el cambio de imagen
+  addBlog(): void {
+    this.title = 'Agregar Blog';
+    this.blogForm.reset();
+    this.showForm = true;
+    this.isEditing = false;
+  }
+
+  editBlog(blog: any): void {
+    this.title = 'Editar Blog';
+    this.blogSelected = blog;
+    this.blogForm = this.fb.group({
+      title: [this.blogSelected.title, Validators.required],
+      image: [this.blogSelected.image],
+      resume: [this.blogSelected.resume, Validators.required],
+      content: [this.blogSelected.content, Validators.required],  
+      category: [this.blogSelected.category, Validators.required]
+    });
+    this.showForm = true;
+    this.isEditing = true;
+  }
+
+  cancelAddBlog(): void {
+    this.showForm = false;
+    this.blogForm.reset();
+  }
+
   onImageChange(event: any): void {
     const file = event.target?.files?.[0];
     if (file) {
@@ -71,6 +122,42 @@ export class HomeComponent implements OnInit {
       };
       reader.readAsDataURL(file);
     }
+  }
+
+  deleteBlog(blog: any): void {
+    this.blogSelected = blog;
+    Swal.fire({
+      title: "Esta seguro de eliminar el Blog?",
+      showCancelButton: true,
+      confirmButtonText: "Confirmar",
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+
+        this.blogService.deleteBlog(this.blogSelected.id).subscribe(
+          (response) => {
+            console.log('Blog eliminado:', response);
+            this.uploadBlogs();
+            Swal.fire({
+              title: 'Información',
+              text: 'El blog se ha eliminado correctamente',
+              icon: 'success',
+              confirmButtonText: 'OK'
+            });
+          },
+          (error) => {
+            Swal.fire({
+              title: 'Error',
+              text: 'Ha ocurrido un error al eliminar el blog',
+              icon: 'error',
+              confirmButtonText: 'OK'
+            })
+          }
+        );
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
   }
 
 }
--- src/app/services/blog.service.ts
+++ src/app/services/blog.service.ts
@@ -3,23 +3,34 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class BlogService {
-    
-    private apiUrl = 'http://localhost:8080/api/blogs';
 
-    constructor(private http: HttpClient) {}
+  private apiUrl = 'http://localhost:8080/api/blogs';
 
-    getBlogs(): Observable<any> {
-        return this.http.get<any>(this.apiUrl);
+  constructor(private http: HttpClient) { }
+
+  getBlogs(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  getBlogDetails(id: number): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/${id}`);
+  }
+
+  updateBlog(id: number, blog: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, blog);
+  }
+
+  saveBlog(blog: any, isEditing:boolean): Observable<any> {
+    if (isEditing) {
+      return this.updateBlog(blog.id, blog);
     }
+    return this.http.post<any>(this.apiUrl, blog);
+  }
 
-    getBlogDetails(id: number): Observable<any> {
-        return this.http.get<any>(`${this.apiUrl}/${id}`);
-      }
-    
-      updateBlog(id: number, blog: any): Observable<any> {
-        return this.http.put<any>(`${this.apiUrl}/${id}`, blog);
-      }
+  deleteBlog(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+}
 }
